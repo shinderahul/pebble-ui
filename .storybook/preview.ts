63117c35deeaa208b6b7bb7f322fae1a09e60c21
shinderahul <rahulbshinde89@gmail.com>
2026-06-18 15:25:38 +0530

    storybook init

---
+++ .storybook/preview.ts
@@ -0,0 +1,12 @@
+import type { Preview } from '@storybook/react-vite';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: { color: /(background|color)$/i, date: /Date$/i },
+    },
+  },
+  tags: ['autodocs'],
+};
+
+export default preview;
