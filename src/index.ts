c525a070758f3c2e31ed8fe8b84bd5f72bb96040
shinderahul <rahulbshinde89@gmail.com>
2026-07-08 23:01:57 +0530

    style issue fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import './styles.scss';
+import '../src/styles/global.scss';
 export { Button } from './components/Button';
 export type { ButtonProps } from './components/Button';
 export { Input } from './components/Input';
